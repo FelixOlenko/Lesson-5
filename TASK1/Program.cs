1003b57e411896b904be9350f8864b03df18bffd
Felix Olenko <0feliks0@gmail.com>
2022-06-24 09:28:26 +0300

    1

---
+++ TASK1/Program.cs
@@ -0,0 +1,38 @@
+﻿          
+          int[] CreateAndFillArr(int large, int min, int max)
+          {
+          int[] array = new int[large];
+          for (int ifill = 0; ifill < array.Length; ifill++)
+          {
+            array[ifill] = new Random().Next(min,max + 1);
+          }
+          return array;
+          }
+          void printArr(int[] arr)
+          {
+          for (int i = 0; i < arr.Length; i++)
+          {
+            Console.Write($"{arr[i]}  ");
+          }
+          System.Console.WriteLine();
+          }
+
+          int SearchEvenNumbers(int[] arr)
+          {
+            int count = 0;
+            for(int i=0; i<arr.Length;i++)
+            {
+                if(arr[i]%2 == 0)
+                {
+                    count++;
+                }
+            }
+            System.Console.WriteLine($"Чётные числа в массиве => {count}");
+            return count;
+          }
+
+          int[] myArray = CreateAndFillArr(10,11,1000);
+          printArr(myArray);
+          SearchEvenNumbers(myArray);
+
+
